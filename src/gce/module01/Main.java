7006c8b6301c8d041b8bf83199bb8f78d464a83a
Queue Stainless <gce517+git@gmail.com>
2020-05-22 13:46:21 -0400

    Added global variables

--- src/gce/module01/Main.java
+++ src/gce/module01/Main.java
@@ -1,6 +1,28 @@
 package gce.module01;
 
+import java.util.Random;
+
 public class Main {
+
+    /**
+     * The Integer values to store in each list
+     */
+    static Integer randomInteger;
+
+    /**
+     * The random number array length.
+     */
+    static final int randomNumberArrayLength = 2000000;
+
+    /**
+     * The random number.
+     */
+    static final Random randomNumber = new Random();
+
+    /**
+     * The maximum value of the random numbers to generate.
+     */
+    static final int randomNumberMaxValue = 10;
 
     public static void main(String[] args) {
 	// write your code here
