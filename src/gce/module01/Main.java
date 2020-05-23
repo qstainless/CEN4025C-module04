8f91f49a090c645847efc053e89c8d6059fc6940
Queue Stainless <gce517+git@gmail.com>
2020-05-23 09:34:24 -0400

    Added processLinkedList() and processHashTable() methods

--- src/gce/module01/Main.java
+++ src/gce/module01/Main.java
@@ -1,6 +1,8 @@
 package gce.module01;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class Main {
@@ -43,24 +45,45 @@ public class Main {
             arrayListValues.add(randomInteger);
         }
 
-        System.out.println("Done adding.");
-
         /*
          There are several ways to remove elements in an ArrayList. We could
          use the clear() or removeAll() methods, use an Iterator to remove
          elements staring with the first index (0), or use a simple while
          loop to remove them starting at the last element.
         */
+        // Remove the numbers from the ArrayList
         while (!arrayListValues.isEmpty()) {
             arrayListValues.remove(arrayListValues.size() - 1);
         }
-
-        System.out.println("Done removing.");
     }
 
     public static void processLinkedList() {
+        LinkedList<Integer> linkedListValues = new LinkedList<>();
+
+        // Add the random numbers to the LinkedList
+        for (int i = 0; i < randomNumberArrayLength; i++){
+            randomInteger = randomNumber.nextInt(randomNumberMaxValue) + 1;
+            linkedListValues.add(randomInteger);
+        }
+
+        // Remove the numbers from the LinkedList
+        while (!linkedListValues.isEmpty()) {
+            linkedListValues.remove(linkedListValues.size() - 1);
+        }
     }
 
     public static void processHashTable() {
+        Hashtable<Integer, Integer> hashtablePairs = new Hashtable<>();
+
+        // Add the random numbers to the Hashtable
+        for (int i = 0; i < randomNumberArrayLength; i++){
+            randomInteger = randomNumber.nextInt(randomNumberMaxValue) + 1;
+            hashtablePairs.put(i, randomInteger);
+        }
+
+        // Remove the numbers from the Hashtable
+        while (!hashtablePairs.isEmpty()) {
+            hashtablePairs.remove(hashtablePairs.size() - 1);
+        }
     }
 }
